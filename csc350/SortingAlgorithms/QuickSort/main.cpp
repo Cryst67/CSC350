c0726d297384c10d1c3fa0c52ece92f5c43648e3
Aryan S <85245228+Cryst67@users.noreply.github.com>
2022-11-17 21:16:23 -0600

    fix quickSort

--- csc350/SortingAlgorithms/QuickSort/main.cpp
+++ csc350/SortingAlgorithms/QuickSort/main.cpp
@@ -1,30 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 using namespace std;
 
+// quick sort = moves smaller elements to left of a pivot.
+    	//  recursively divide array in 2 partitions
+
+    	//  run-time complexity = Best case O(n log(n))
+    	//  Average case O(n log(n))
+    	//  Worst case O(n^2) if already sorted
+    	
+    	//  space complexity    = O(log(n)) due to recursion
+
+void quickSort(int arr[], int start, int end);
+int partition(int arr[], int start, int end);
+
 int main(){
-
-    unordered_map<char, vector<char>> graph;
-    graph['a'] = {'b', 'c'};
-    graph['b'] = {'d'};
-    graph['c'] = {'e'};
-    graph['d'] = {'f'};
-    graph['e'] = {};
-    graph['f'] = {};
-
-    vector<char> queue = {'a'};
     
-    while(queue.size() > 0){
-        char cur = queue.front();
-        cout << cur << ' ';
-        queue.erase(queue.begin());
-        for(auto neighbor : graph[cur]){
-            queue.push_back(neighbor);
+    int arr[]{8,5,9,1,4,2,7,6,3};
+    quickSort(arr, 0, size(arr)-1);
+    for(auto i : arr) cout << i << ' ';
+    cout << '\n';
+    return 0;
+}
+
+void quickSort(int arr[], int start, int end){
+    if(end <=  start) return; 
+    int pivot = partition(arr, start, end);
+    quickSort(arr, start, pivot-1);
+    quickSort(arr, pivot + 1, end);
+}
+
+int partition(int arr[], int start, int end){
+    int pivot = arr[end];
+    int i = start - 1;
+    for(int j = start; j <= end -1; j++){
+        if(arr[j] < pivot){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
-    cout << '\n';
+    i++;
+    int temp = arr[i];
+    arr[i] = arr[end];
+    arr[end] = temp;
 
-
-    return 0;
+    return i;
 }
